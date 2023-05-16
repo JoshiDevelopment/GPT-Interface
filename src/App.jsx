2cf76744f6d77577d0bec307632c47151594699d
JoshiDevelopment <joshuamcrawford999@gmail.com>
2023-05-15 18:45:43 -0700

    image uploaded

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import Logo from './images/liquid-logo-design-vector-27243616.png';
 function App() {
   return (
     <div className="main">
+      <div className="logo-container">
       <img src={Logo} alt="Logo"/>
+      </div>
+      
+      
+      
     </div>
   );
 }
