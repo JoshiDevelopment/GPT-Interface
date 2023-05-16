fa88502c99352c846132009b5e555a3e24e16415
JoshiDevelopment <joshuamcrawford999@gmail.com>
2023-05-15 18:27:34 -0700

    fixed background issue (sorta)

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,6 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <div className="main">
-      <div className="sub-main">  
-    </div>
-    </div>
       
     </div>
   );
